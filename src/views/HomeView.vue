d80e55b57b493eebc2449606fb65e607028b9216
A.Rahman Al-Khateeb <khateeboveskey@gmail.com>
2024-11-20 10:34:56 +0300

    add projects section

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup>
 import CarouselComp from '@/components/CarouselComp.vue';
 import SectionAbout from '@/components/SectionAbout.vue';
+import SectionProjects from '@/components/SectionProjects.vue';
 </script>
 
 <template>
   <CarouselComp />
   <SectionAbout />
+  <SectionProjects />
 </template>
