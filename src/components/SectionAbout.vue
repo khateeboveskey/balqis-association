e033bbfbcbd7487a3bf1ac75bccc7f11f6160cf1
A.Rahman Al-Khateeb <khateeboveskey@gmail.com>
2024-12-09 11:02:41 +0300

    refactor: make cards in a json

--- src/components/SectionAbout.vue
+++ src/components/SectionAbout.vue
@@ -1,26 +1,6 @@
 <script setup>
 import SectionHeading from './SectionHeading.vue';
-
-const aboutCards = [
-  {
-    icon: 'fa-solid fa-bullseye',
-    title: 'أهدافنا',
-    description:
-      'تهدف الجمعية إلى خلق فرص عمل مستدامة، رفع مستوى الوعي والتعليم، تحسين الأداء المؤسسي، تشكيل شراكات متعددة، وتقديم خدمات ذات جودة للمجتمع المحلي.'
-  },
-  {
-    icon: 'fa-regular fa-eye',
-    title: 'رؤيتنا',
-    description:
-      'أن تكون جمعية بلقيس هي الجمعية الرائدة في مجال تنمية وتدريب وتأهيل المرأة وحماية الطفل وبناء السلام المجتمعي بحلول عام 2025م في مديريــة طور الباحة'
-  },
-  {
-    icon: 'fa-regular fa-envelope',
-    title: 'رسالتنا',
-    description:
-      'جمعية بلقيس للمرأة والطفل جمعية نسوية طوعية تنموية، لا تهدف للربح تعمل في مجال التنمية المستدامة للوصول الى أعلى معدلات النماء في مديرية طور الباحة'
-  }
-];
+import { cards } from '@/database/cards.json';
 </script>
 
 <template>
@@ -28,7 +8,7 @@ const aboutCards = [
     <SectionHeading>عن الجمعية</SectionHeading>
     <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mb-8">
       <div
-        v-for="card in aboutCards"
+        v-for="card in cards"
         :key="card.title"
         class="p-8 bg-primary-50 rounded-lg border border-primary-300"
       >
