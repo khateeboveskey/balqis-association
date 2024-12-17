fb50b8019cb76a1d2d5ab2957e45ccda17770d44
A.Rahman Al-Khateeb <khateeboveskey@gmail.com>
2024-12-17 21:04:04 +0300

    perf(build): add `vite-plugin-image-optimizer`

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,25 @@
 import { fileURLToPath, URL } from 'node:url';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
   base: '/',
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    ViteImageOptimizer({
+      png: {
+        quality: 30
+      },
+      jpeg: {
+        quality: 30
+      },
+      jpg: {
+        quality: 30
+      }
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
