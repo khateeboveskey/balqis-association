f46c5b192021d12242f6bfd50b4423456a8282a8
A.Rahman Al-Khateeb <khateeboveskey@gmail.com>
2025-03-18 17:48:42 +0300

    refactor: initial migration

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -3,15 +3,10 @@
     <div class="mx-auto max-w-screen-xl">
       <div class="md:flex md:justify-between">
         <div class="mb-6 md:mb-0">
-          <RouterLink to="/" class="flex items-center space-x-4 rtl:space-x-reverse -translate-y-5">
-            <img
-              loading="lazy"
-              src="/imgs/logo.png"
-              class="h-16 self-center"
-              alt="شعار جمعية بلقيس"
-            />
+          <NuxtLink to="/" class="flex items-center space-x-4 rtl:space-x-reverse -translate-y-5">
+            <img loading="lazy" src="/logo.png" class="h-16 self-center" alt="شعار جمعية بلقيس" />
             <span class="self-center text-2xl font-bold">جمعية بلقيس للمرأة والطفل</span>
-          </RouterLink>
+          </NuxtLink>
         </div>
         <div :class="['grid grid-cols-1 gap-8', `sm:grid-cols-${Object.keys(footerLinks).length}`]">
           <div v-for="(footerList, title) in footerLinks" :key="title">
@@ -20,12 +15,7 @@
             </h2>
             <ul class="text-gray-600">
               <li v-for="link in footerList" :key="link.url" class="mb-4">
-                <a
-                  target="_blank"
-                  :href="link.url"
-                  class="hover:underline hover:text-primary-800"
-                  >{{ link.text }}</a
-                >
+                <a target="_blank" :href="link.url" class="hover:underline hover:text-primary-800">{{ link.text }}</a>
               </li>
             </ul>
           </div>
@@ -35,14 +25,7 @@
       <div class="flex items-center justify-between">
         <span class="text-sm text-gray-500 sm:text-center">{{ currentYear }}</span>
         <div class="flex gap-5 sm:justify-center sm:mt-0">
-          <a
-            v-for="social in socialMedia"
-            :key="social.url"
-            target="_blank"
-            :href="social.url"
-            :aria-label="social.ariaLabel"
-            class="text-gray-500 hover:text-gray-900"
-          >
+          <a v-for="social in socialMedia" :key="social.url" target="_blank" :href="social.url" :aria-label="social.ariaLabel" class="text-gray-500 hover:text-gray-900">
             <i :class="social.icon" class="w-5 h-5"></i>
           </a>
         </div>
---
+++ layouts/default.vue
@@ -0,0 +1,20 @@
+<template>
+  <AppHeader />
+  <slot />
+  <AppFooter />
+</template>
+
+<script setup lang="ts">
+useHead({
+  title: 'الرئيسية',
+  meta: [
+    { name: 'author', content: 'جمعية بلقيس للمرأة والطفل' },
+    { name: 'description', content: 'جمعية بلقيس للمرأة والطفل جمعية نسوية طوعية تنموية، لا تهدف للربح تعمل في مجال التنمية المستدامة للوصول الى أعلى معدلات النماء في مديرية طور الباحة' },
+    { name: 'keywords', content: 'جمعية, بلقيس, مرأة, طفل, جمعية نسوية, جمعية تنموية, طور الباحة' }
+  ],
+  link: [
+    { rel: 'icon', href: 'logo.ico' },
+    { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css', integrity: 'sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==', crossorigin: 'anonymous', referrerpolicy: 'no-referrer' }
+  ],
+  titleTemplate: '%s | جمعية بلقيس للمرأة والطفل',
+});</script>
