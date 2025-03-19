8f8894f5385a7ca1d58c9f20090c1a995fc074ff
A.Rahman Al-Khateeb <khateeboveskey@gmail.com>
2025-03-19 17:09:32 +0300

    chore: some changes:
    - remove unused images and update image paths
    - add Tailwind CSS configuration

--- components/ArticleCard.vue
+++ components/ArticleCard.vue
@@ -9,16 +9,9 @@ defineProps({
 
 <template>
   <div v-for="article in articles" :key="article.id">
-    <img
-      loading="lazy"
-      :src="article.image"
-      class="w-full h-auto rounded-t-lg"
-      :alt="article.title"
-    />
+    <NuxtImg quality="50" format="webp" loading="lazy" :src="article.image" class="w-full h-auto rounded-t-lg" :alt="article.title" />
     <div class="rounded-b-lg space-y-4 bg-primary-50 p-5 border border-primary-300">
-      <span
-        class="bg-primary-100 text-primary-800 border border-primary-500 text-xs font-medium inline-flex items-center px-2.5 py-1 rounded"
-      >
+      <span class="bg-primary-100 text-primary-800 border border-primary-500 text-xs font-medium inline-flex items-center px-2.5 py-1 rounded">
         {{ article.tag }}
       </span>
       <h3 class="text-2xl font-bold leading-tight text-gray-900">
@@ -27,13 +20,7 @@ defineProps({
       <p class="font-normal text-gray-500">
         {{ article.description }}
       </p>
-      <a
-        v-if="article.page"
-        href="#"
-        title=""
-        class="duration-150 transition-all text-white bg-primary-600 justify-center hover:bg-primary-800 inline-flex items-center focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center :bg-primary-700 :ring-primary-800"
-        role="button"
-      >
+      <a v-if="article.page" href="#" title="" class="duration-150 transition-all text-white bg-primary-600 justify-center hover:bg-primary-800 inline-flex items-center focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center :bg-primary-700 :ring-primary-800" role="button">
         المزيد
       </a>
     </div>
--- components/CarouselComp.vue
+++ components/CarouselComp.vue
@@ -3,8 +3,8 @@
     <!-- Carousel wrapper -->
     <div class="relative h-56 overflow-hidden rounded-lg md:h-96">
       <div v-for="(slide, index) in slides" :key="index" class="hidden duration-700 ease-in-out" data-carousel-item>
-        <img loading="lazy" :src="slide.imageUrl" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="" />
-        <div class="carousel-caption text-center h-full grid place-items-center relative bg-opacity-50 bg-black">
+        <NuxtImg quality="50" format="webp" loading="lazy" :src="slide.imageUrl" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" :alt="slide.heading" />
+        <div class="carousel-caption text-center h-full grid place-items-center relative bg-black/75">
           <div class="flex flex-col gap-2 items-center">
             <h3 class="inline-block translate-x-0 font-bold bg-primary-800 px-2 py-1 rounded-md text-white">
               {{ slide.heading }}
@@ -17,12 +17,11 @@
       </div>
     </div>
     <!-- Slider indicators -->
-    <!-- todo: fix direction -->
     <div class="absolute z-30 flex -translate-x-1/2 space-x-3 rtl:space-x-reverse bottom-5 left-1/2">
       <button v-for="(slide, index) in slides" :key="index" type="button" class="w-3 h-3 rounded-full" :aria-current="index + 1" :aria-label="`Slide ${index + 1}`" :data-carousel-slide-to="index"></button>
     </div>
     <!-- Slider controls -->
-    <button type="button" class="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
+    <button type="button" class="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
       <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 group-hover:bg-white/50 group-focus:ring-4 group-focus:ring-white group-focus:outline-none">
         <svg class="w-4 h-4 text-white rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
           <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4" />
@@ -30,7 +29,7 @@
         <span class="sr-only">السابق</span>
       </span>
     </button>
-    <button type="button" class="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
+    <button type="button" class="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
       <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 group-hover:bg-white/50 group-focus:ring-4 group-focus:ring-white group-focus:outline-none">
         <svg class="w-4 h-4 text-white rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
           <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4" />
@@ -44,32 +43,31 @@
 <script setup>
 import { initFlowbite } from 'flowbite'
 
-// initialize components based on data attribute selectors
 onMounted(() => {
   initFlowbite();
 })
 
 const slides = [
   {
-    imageUrl: 'assets/imgs/1.jpg',
+    imageUrl: '/imgs/1.jpg',
     heading: 'جمعية بلقيس للمرأة والطفل',
     paragraph:
       'جمعية بلقيس جمعية نسوية تأسست في 26 ابريل 2005م تعمل في مجال التنمية المستدامة للمجتمع وخاصة شريحة النساء والأطفال'
   },
   {
-    imageUrl: 'assets/imgs/2.jpg',
+    imageUrl: '/imgs/2.jpg',
     heading: 'المرأة اساس المجتمع',
     paragraph:
       'جمعية بلقيس جمعية نسوية تأسست في 26 ابريل 2005م تعمل في مجال التنمية المستدامة للمجتمع وخاصة شريحة النساء والأطفال'
   },
   {
-    imageUrl: 'assets/imgs/3.jpg',
+    imageUrl: '/imgs/3.jpg',
     heading: 'بالوعي ترقى الامة',
     paragraph:
       'جمعية بلقيس جمعية نسوية تأسست في 26 ابريل 2005م تعمل في مجال التنمية المستدامة للمجتمع وخاصة شريحة النساء والأطفال'
   },
   {
-    imageUrl: 'assets/imgs/4.jpg',
+    imageUrl: '/imgs/4.jpg',
     heading: 'المرأة اساس المجتمع',
     paragraph:
       'جمعية بلقيس جمعية نسوية تأسست في 26 ابريل 2005م تعمل في مجال التنمية المستدامة للمجتمع وخاصة شريحة النساء والأطفال'
