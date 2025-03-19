403b60eb4ba67b9670a7b190824f3e7be999901e
A.Rahman Al-Khateeb <khateeboveskey@gmail.com>
2025-03-19 17:30:36 +0300

    feat: add error page

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,5 +1,5 @@
 <template>
-  <footer class="w-10/12 mx-auto pb-5 pt-10">
+  <footer class="pb-5 pt-14 mt-20 bg-neutral-900 text-white w-full px-4 sm:px-6 lg:px-8">
     <div class="mx-auto max-w-screen-xl">
       <div class="md:flex md:justify-between">
         <div class="mb-6 md:mb-0">
@@ -10,10 +10,10 @@
         </div>
         <div :class="['grid grid-cols-1 gap-8', `sm:grid-cols-${Object.keys(footerLinks).length}`]">
           <div v-for="(footerList, title) in footerLinks" :key="title">
-            <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase">
+            <h2 class="mb-6 text-sm font-semibold uppercase">
               {{ title }}
             </h2>
-            <ul class="text-gray-600">
+            <ul class="text-neutral-400">
               <li v-for="link in footerList" :key="link.url" class="mb-4">
                 <a target="_blank" :href="link.url" class="hover:underline hover:text-primary-800">{{ link.text }}</a>
               </li>
@@ -21,11 +21,11 @@
           </div>
         </div>
       </div>
-      <hr class="my-6 border-gray-200 sm:mx-auto lg:my-8" />
+      <hr class="my-6 border-neutral-700 sm:mx-auto lg:my-8" />
       <div class="flex items-center justify-between">
-        <span class="text-sm text-gray-500 sm:text-center">{{ currentYear }}</span>
+        <span class="text-sm text-neutral-500 sm:text-center">{{ currentYear }}</span>
         <div class="flex gap-5 sm:justify-center sm:mt-0">
-          <a v-for="social in socialMedia" :key="social.url" target="_blank" :href="social.url" :aria-label="social.ariaLabel" class="text-gray-500 hover:text-gray-900">
+          <a v-for="social in socialMedia" :key="social.url" target="_blank" :href="social.url" :aria-label="social.ariaLabel" class="text-neutral-500 hover:text-neutral-600">
             <i :class="social.icon" class="w-5 h-5"></i>
           </a>
         </div>
@@ -35,9 +35,6 @@
 </template>
 
 <script setup>
-import { computed } from 'vue';
-import { RouterLink } from 'vue-router';
 import { footerLinks, socialMedia } from '@/database/links.json';
-
 const currentYear = computed(() => new Date().getFullYear());
 </script>
--- error.vue
+++ error.vue
@@ -1,25 +1,29 @@
+<script setup lang="ts">
+import type { NuxtError } from '#app'
+
+const props = defineProps({
+  error: Object as () => NuxtError
+})
+
+useHead({
+  title: 'خطأ',
+})
+</script>
 
 <template>
-  <NuxtLayout>
-    <div class="min-h-screen flex items-center justify-center bg-gray-50" dir="rtl">
-      <div class="text-center">
-        <h1 class="text-6xl font-bold text-red-600 mb-4">{{ error.statusCode }}</h1>
-        <h2 class="text-2xl font-semibold text-gray-800 mb-4">عذراً، حدث خطأ ما</h2>
-        <p class="text-gray-600 mb-8">
-          {{ error.statusCode === 404 ? 'الصفحة المطلوبة غير موجودة' : 'حدث خطأ في الخادم' }}
+  <NuxtLayout class="font-sans bg-accent-background min-h-screen w-full">
+    <div class="container mx-auto px-4 min-h-screen flex items-center justify-center">
+      <div class="text-center max-w-2xl mx-auto py-8 px-4">
+        <h1 class="text-[clamp(7rem,15vw,12rem)] font-extrabold tracking-wider leading-tight mb-4">
+          {{ error?.statusCode || '404' }}
+        </h1>
+        <p class="text-[clamp(1.25rem,4vw,2rem)] mb-8 opacity-90">
+          عذراً، {{ error?.statusCode === 404 ? 'الصفحة غير موجودة' : 'حصل خطأ في الخادم' }}
         </p>
-        <NuxtLink
-          to="/"
-          class="bg-red-600 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-lg transition duration-300">
-          العودة إلى الصفحة الرئيسية
+        <NuxtLink class="inline-block text-white bg-primary-600 rounded-lg hover:opacity-90 transition-opacity duration-300 px-6 py-3 md:text-lg" to="/">
+          العودة للرئيسية
         </NuxtLink>
       </div>
     </div>
   </NuxtLayout>
 </template>
-
-<script setup>
-const props = defineProps({
-  error: Object
-})
-</script>
